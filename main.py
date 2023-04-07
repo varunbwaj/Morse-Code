4467ceb95942c1ed89df8495e3bd6fe3c4c29ba8
Varun Bharadwaj <varunbwaj@gmail.com>
2023-04-08 00:05:53 +0530

    Made quite a lot of changes to the GUI and fonts
    Making it easy on the eyes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def morse(morse):
     output_text.delete('1.0', tk.END)       #Clear output section
     alpha = f.morse_to_alphanumeric(morse)  #calling the conversion function
     alpha_text = translator.translate(text=alpha,dest=DEST).text
-    print(alpha_text)
+    # print(alpha_text)
     output_text.insert(1.0, alpha_text)          #Display the output
 
 
@@ -41,6 +41,7 @@ languages_dict = {"en":"English","bn": "Bengali",
                   "ta": "Tamil",  "te": "Telugu",
                   "ur" :"Urdu"}
 
+
 def UpdateDEST(event):
     global languages_dict
     global DEST
@@ -48,43 +49,65 @@ def UpdateDEST(event):
 
 #create the tkinter window
 root = tk.Tk()
-
-#created GUI widgets
-canvas = tk.Canvas(root, height=800, width=800)
-head_label = tk.Label(canvas, text='Morse Converter', font=('verdana', 25))
-notice_frame = tk.Frame(canvas, height=200, width=550, bd=10, bg='#a0a0a0')
-input_notice1 = tk.Label(notice_frame, text='The input is accepted in Morse or Alpha-Numeric.', bg='#a0a0a0',
-                         font=('verdana', 10))
-input_notice2 = tk.Label(notice_frame,
-                         text='If input is in morse then leave one space between each character and two space between each word.',
-                         bg='#a0a0a0', font=('verdana', 10))
-input_label = tk.Label(canvas, text='Input: ', font=('verdana', 20))
-input_entry = tk.Entry(canvas, font=('verdana', 13), width=58)
-output_label = tk.Label(canvas, text='Output ↓', font=('verdana', 20))
-output_text = tk.Text(canvas, height=9, width=56, borderwidth=1, font=('verdana', 15))
-morse_alpha_button = tk.Button(canvas, text='Morse → AlphaNumeric', font=('verdana', 15),
-                               command=lambda: morse(input_entry.get()))
-alpha_morse_button = tk.Button(canvas, text='AlphaNumeric → Morse', font=('verdana', 15),
-                               command=lambda: alpha(input_entry.get()))
-play_button = tk.Button(canvas, text='Play\nMorse', font=('verdana', 15), height=3,
-                        command=lambda: play(input_entry.get()))
 clicked = tk.StringVar()
 clicked.set(list(languages_dict.values())[0])
+
+#Creating GUI widgets
+canvas = tk.Canvas(root, height=700, width=750)
+
+head_label = tk.Label(canvas, text='Morse Converter', font=('Times New Roman', 30))
+
+notice_frame = tk.Frame(canvas, height=200, width=550, bd=10, bg='#a0a0a0')
+
+input_notice1 = tk.Label(notice_frame, text='The input is accepted in Morse or Alpha-Numeric.', bg='#a0a0a0',
+                         font=('Times New Roman', 15))
+
+input_notice2 = tk.Label(notice_frame,
+                         text='If input is in morse then leave one space between each character and two space between each word.',
+                         bg='#a0a0a0', font=('Times New Roman', 15))
+
+input_label = tk.Label(canvas, text='Input: ', font=('Helvetica', 20))
+
+input_entry = tk.Entry(canvas, font=('Helvetica', 13),width=55)
+
+output_label = tk.Label(canvas, text='Output ↓', font=('Helvetica', 20))
+
+output_text = tk.Text(canvas, height=5, width=55, borderwidth=1, font=('Helvetica', 15))
+
+morse_alpha_button = tk.Button(canvas, text='Morse → AlphaNumeric', font=('verdana', 15),
+                               command=lambda: morse(input_entry.get()))
+
+alpha_morse_button = tk.Button(canvas, text='AlphaNumeric → Morse', font=('verdana', 15),
+                               command=lambda: alpha(input_entry.get()))
+
+play_button = tk.Button(canvas, text='Play\nMorse', font=('verdana', 15), height=3,
+                        command=lambda: play(input_entry.get()))
+
+notice_frame_1 = tk.Frame(canvas, height=80, width=245, bd=10, bg='#a0a0a0')
+
+input_label_1 = tk.Label(canvas,text="Select Output Language for your Morse-Code:",font=('Helvetica',20))
+
+input_notice3 = tk.Label(notice_frame_1,text="Select the language you want your morse code to be translated into:\n(English is set as default)",
+                         bg='#a0a0a0',font=('Times New Roman',15))
+
 dropDownBox = tk.OptionMenu(canvas,clicked, *list(languages_dict.values()),command=UpdateDEST)
 
 #placing the GUI widgets
 canvas.pack()
 head_label.place(y=2, relx=0.35)
-notice_frame.place(y=55, x=25)
+notice_frame.place(y=55, x=75)
 input_notice1.pack()
 input_notice2.pack()
-input_label.place(y=150, x=5)
-input_entry.place(y=160, x=95)
-output_label.place(y=225, x=5)
-output_text.place(y=275, x=10)
-alpha_morse_button.place(y=520, x=375)
-morse_alpha_button.place(y=565, x=375)
-play_button.place(y=520, x=645)
-dropDownBox.place(y=620, x=42)
+input_label.place(y=150, x=25)
+input_entry.place(y=150, x=95)
+output_label.place(y=225, x=25)
+output_text.place(y=265, x=25)
+alpha_morse_button.place(y=520, x=75)
+morse_alpha_button.place(y=565, x=75)
+play_button.place(y=525, x=350)
+input_label_1.place(y=380,x=15)
+notice_frame_1.place(y=420,x=12)
+input_notice3.pack()
+dropDownBox.place(y=440, x=475)
 
 root.mainloop()
